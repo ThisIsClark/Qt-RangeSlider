b1b9978faa2aad656d124ef8259b56e276df79e7
wilman <wz@prevt.com>
2019-11-11 17:45:29 +0800

    remove dummy codes

--- RangeSlider.cpp
+++ RangeSlider.cpp
@@ -172,15 +172,10 @@ void RangeSlider::mouseReleaseEvent(QMouseEvent* aEvent)
 {
     Q_UNUSED(aEvent);
 
-    // for sliding finish signal
-    //if(1||mFirstHandlePressed || mSecondHandlePressed){
     emit slidingFinished();
-    //}
 
     mFirstHandlePressed = false;
     mSecondHandlePressed = false;
-
-
 }
 
 void RangeSlider::changeEvent(QEvent* aEvent)
